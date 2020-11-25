d488884575a18bb7a873008291d2a8d08ece124f
Paulo Alencar <pauloalencar.aluno@gmail.com>
2020-11-24 21:25:10 -0300

    Fazendo o commitda  aula06 e aula07

---
+++ aula06/aula06.py
@@ -0,0 +1,19 @@
+"""
+
+Iniciar com letra Ou _ (underline)
+pode conter números, separar com _ e conter letras MAIÚSCULAS
+
+"""
+
+nome = 'Paulo'
+idade = 20
+altura = 1.80
+e_maior = idade > 18
+peso = 80
+imc = peso / altura ** 2
+
+print('Nome: ', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior de idade?',e_maior)
+print(nome, ',que possui altura ', altura, ' e peso ', peso, 'kg, tem imc = ', imc)
